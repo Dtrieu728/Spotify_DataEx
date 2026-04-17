a1b29a8bcfe9b550a26d7ac928289a4b34452dd6
Dustine Trieu <dtrieu7679@gmail.com>
2026-04-17 16:35:39 -0400

    testing

--- frontend/src/Components/Login/auth.js
+++ frontend/src/Components/Login/auth.js
@@ -22,9 +22,7 @@ function generateRandomString(length) {
 async function sha256(plain) {
     const encoder = new TextEncoder();
     const data = encoder.encode(plain);
-    const hash = await crypto.subtle.digest("SHA-256", data);
-    const hashArray = Array.from(new Uint8Array(hash));
-    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return await crypto.subtle.digest("SHA-256", data);
 }
 
 function base64encode(input) {
